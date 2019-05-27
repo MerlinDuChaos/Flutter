1b138039bb839b49be56bfaa1d81c75958ef888e
MerlinDuChaos <barbier.j@gmail.com>
2019-05-27 22:56:27 +0200

    Added operations

--- Test1/lib/main.dart
+++ Test1/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(TestJulien());
@@ -9,7 +11,7 @@ class TestJulien extends StatelessWidget {
       title: 'Test Julien',
       theme: ThemeData(brightness: Brightness.dark),
       home: TestJulienHomepage(
-        title: 'TestJulien Homepage',
+        title: 'Apprendre à calculer',
       ),
     );
   }
@@ -26,60 +28,109 @@ class TestJulienHomepage extends StatefulWidget {
   }
 }
 
-class TestButton extends StatelessWidget {
-  final String label;
-  final VoidCallback onClicked;
-  TestButton(this.label, [this.onClicked]);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-      margin: EdgeInsets.all(5),
-      color: Colors.red,
-      child: FlatButton(
-        child: Text(
-          label,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-          ),
-        ),
-        onPressed: this.onClicked,
-      ),
-    );
-  }
-}
-
-TextStyle _displayStyle =
+TextStyle _inputStyle =
     new TextStyle(fontWeight: FontWeight.bold, fontSize: 40);
 
+TextStyle _buttonStyle =
+    new TextStyle(fontWeight: FontWeight.bold, fontSize: 20);
+
 class _TestJulienState extends State<TestJulienHomepage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   Widget buildNumberButtons(num value) {
     return FlatButton(
-        child: Text(value.toString()), onPressed: () => _addToDisplay(value));
+        child: Text(value.toString(), style: _buttonStyle),
+        onPressed: () => _addToDisplay(value));
+  }
+
+  @override
+  initState() {
+    super.initState();
+
+    _selectNewOperation();
+  }
+
+  _TestJulienState() {
+    //_selectNewOperation();
+  }
+
+  _selectNewOperation() {
+    Random r = new Random();
+    int test = r.nextInt(2);
+
+    if (test % 2 == 0) {
+      // Addition
+      String operand = '+';
+      int left = r.nextInt(10);
+      int right = r.nextInt(10);
+
+      expectedResult = left + right;
+      setState(() {
+        operation = '$left $operand $right';
+      });
+    } else {
+      // soustraction
+      String operand = '-';
+      int left = r.nextInt(10);
+      int right = r.nextInt(10);
+
+      if (left >= right) {
+        operation = '$left $operand $right';
+        expectedResult = left - right;
+      } else {
+        operation = '$right $operand $left';
+        expectedResult = right - left;
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(title: Text(widget.title)),
       body: Column(
         children: <Widget>[
           // display
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[Text('$display', style: _displayStyle)],
+            children: <Widget>[
+              Expanded(
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      decoration:
+                          BoxDecoration(border: Border.all(color: Colors.grey)),
+                      alignment: AlignmentDirectional.center,
+                      child: Text('$operation', style: _inputStyle)))
+            ],
+          ),
+
+          // display
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      decoration: BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(color: Colors.blueAccent))),
+                      alignment: AlignmentDirectional.center,
+                      child: Text('$input', style: _inputStyle)))
+            ],
           ),
 
           // 1 -> 3
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              buildNumberButtons(1),
-              buildNumberButtons(2),
-              buildNumberButtons(3),
-            ],
-          ),
+          Container(
+              padding: const EdgeInsets.only(top: 50),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  buildNumberButtons(1),
+                  buildNumberButtons(2),
+                  buildNumberButtons(3),
+                ],
+              )),
 
           // 4 -> 6
           Row(
@@ -105,7 +156,7 @@ class _TestJulienState extends State<TestJulienHomepage> {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              FlatButton(child: Text('0'), onPressed: () => _addToDisplay(0))
+              buildNumberButtons(0),
             ],
           ),
 
@@ -113,197 +164,55 @@ class _TestJulienState extends State<TestJulienHomepage> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              FlatButton(child: Text('Effacer'), onPressed: () => _clear()),
-              FlatButton(child: Text('Envoyer'), onPressed: () => {})
+              FlatButton(child: Text('Effacer', style: _buttonStyle), onPressed: () => _clear()),
+              FlatButton(
+                  child: Text('Vérifier', style: _buttonStyle),
+                  onPressed: () => input != '' ? _compare() : null)
             ],
           ),
-
-          /*
-          Container(
-              color: Colors.blue,
-              child: Column(children: <Widget>[
-                Row(children: <Widget>[
-                  Expanded(
-                      child: Container(
-                          child: Text('$display',
-                              style: TextStyle(
-                                backgroundColor: Colors.cyan,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 40,
-                              ))))
-                ]),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      TestButton('1', () => _addToDisplay(1)),
-                      TestButton('2', () => _addToDisplay(2)),
-                      TestButton('3', () => _addToDisplay(3)),
-                    ]),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      TestButton('4', () => _addToDisplay(4)),
-                      TestButton('5', () => _addToDisplay(5)),
-                      TestButton('6', () => _addToDisplay(6)),
-                    ]),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      TestButton('7', () => _addToDisplay(7)),
-                      TestButton('8', () => _addToDisplay(8)),
-                      TestButton('9', () => _addToDisplay(9)),
-                    ]),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      TestButton('0', () => _addToDisplay(0)),
-                    ]),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      TestButton('Effacer', () => _clear()),
-                      TestButton('Envoyer', () => {}),
-                    ]),
-              ]))
-              */
         ],
       ),
     );
   }
 
+  final errorSnack = SnackBar(
+      backgroundColor: Colors.red,
+      content: Text(
+        'Erreur - Essaye encore !',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      ));
+
+  final successSnack = SnackBar(
+      duration: Duration(seconds: 1),
+      backgroundColor: Colors.green,
+      content: Text(
+        'Bien joué !',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      ));
+
   _clear() {
     setState(() {
-      display = '';
+      input = '';
     });
+  }
+
+  _compare() {
+    if (int.parse(input) == expectedResult) {
+      _scaffoldKey.currentState.showSnackBar(successSnack);
+      _clear();
+      _selectNewOperation();
+    } else {
+      _scaffoldKey.currentState.showSnackBar(errorSnack);
+    }
   }
 
   _addToDisplay(int value) {
     setState(() {
-      display = display + value.toString();
+      input = input + value.toString();
     });
   }
 
-  String display = '';
-
-  // Widget testButton = Container(
-  //   child: Text(
-
-  //   ),
-  // );
-
+  String input = '';
+  String operation = '';
+  int expectedResult = 0;
 }
-
-// import 'package:flutter/material.dart';
-
-// //void main() => runApp(MyApp());
-
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // Try running your application with "flutter run". You'll see the
-//         // application has a blue toolbar. Then, without quitting the app, try
-//         // changing the primarySwatch below to Colors.green and then invoke
-//         // "hot reload" (press "r" in the console where you ran "flutter run",
-//         // or simply save your changes to "hot reload" in a Flutter IDE).
-//         // Notice that the counter didn't reset back to zero; the application
-//         // is not restarted.
-//         primarySwatch: Colors.blue,
-//         brightness: Brightness.dark
-//         //accentColor: Colors.yellow
-//         //typography: Typography.blackMountainView
-//       ),
-//       home: MyHomePage(title: 'Flutter Demo Julien'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key key, this.title}) : super(key: key);
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug painting" (press "p" in the console, choose the
-//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//           // to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'You have clicked the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.display1,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
